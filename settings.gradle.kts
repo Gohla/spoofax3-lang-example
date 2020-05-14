e728d8bb28d11b6189e414aa4faee0d94732ece0
Gabriel Konat <gabrielkonat@gmail.com>
2020-05-14 11:41:00 +0200

    Update versions and Eclipse plugins are now supported.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,14 +6,12 @@ pluginManagement {
   }
 }
 
+enableFeaturePreview("GRADLE_METADATA")
+
 include("mod.spoofax2")
 include("mod")
 include("mod.spoofax")
 include("mod.cli")
-/*
-Eclipse plugin is disabled for now: the Coronium Gradle plugin does not properly export its configurations when
-published, so a dependency to a published version does not work properly.
-*/
-//include("mod.eclipse.externaldeps")
-//include("mod.eclipse")
+include("mod.eclipse.externaldeps")
+include("mod.eclipse")
 include("mod.intellij")
