0654a67d581373db31e1992989d8bb95a3c9c00c
Gabriel Konat <gabrielkonat@gmail.com>
2020-05-18 15:03:56 +0200

    Update to newest version of Spoofax 3.

--- mod.spoofax2/build.gradle.kts
+++ mod.spoofax2/build.gradle.kts
@@ -1,12 +1,7 @@
 plugins {
-  id("org.metaborg.spoofax.gradle.langspec") version "0.2.4"
+  id("org.metaborg.spoofax.gradle.langspec")
   id("de.set.ecj") // Use ECJ to speed up compilation of Stratego's generated Java files.
   `maven-publish`
-}
-
-spoofax {
-  metaborgVersion = "2.5.8"
-  createPublication = true
 }
 
 ecj {
