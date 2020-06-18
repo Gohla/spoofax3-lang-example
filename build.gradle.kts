38f92d3a3275958c08423f10d3b4de15746b130c
Gabriel Konat <gabrielkonat@gmail.com>
2020-06-18 09:46:38 +0200

    Update plugin versions.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,11 +2,11 @@ plugins {
   id("org.metaborg.gitonium") version "0.1.3"
 
   // Set versions for plugins to use, only applying them in subprojects (apply false here).
-  id("org.metaborg.spoofax.compiler.gradle.spoofaxcore.language") version "0.1.6" apply false
-  id("org.metaborg.spoofax.gradle.langspec") version "0.3.7" apply false
+  id("org.metaborg.spoofax.compiler.gradle.spoofaxcore.language") version "0.1.7" apply false
+  id("org.metaborg.spoofax.gradle.langspec") version "0.3.10" apply false
   id("de.set.ecj") version "1.4.1" apply false
   id("org.metaborg.coronium.bundle") version "0.3.3" apply false
-  id("biz.aQute.bnd.builder") version "5.0.1" apply false
+  id("biz.aQute.bnd.builder") version "5.1.1" apply false
   id("org.jetbrains.intellij") version "0.4.21" apply false
 }
 
